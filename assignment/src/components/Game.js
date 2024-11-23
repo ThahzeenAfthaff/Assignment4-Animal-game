eaaa481cddadfc499800228d721f5d929e513fb8
ThahzeenAfthaff <139678210+ThahzeenAfthaff@users.noreply.github.com>
2024-11-23 15:12:03 +0000

    components added

---
+++ assignment/src/components/Game.js
@@ -0,0 +1,67 @@
+import animals from "../data/AnimalDb";
+import "../assets/game.css";
+import { useState } from "react";
+
+export default function Game(){
+    const [targetAnimal, setTargetAnimal] = useState(getRandomAnimal());
+    const [result, setResult] = useState(null);
+
+    function getRandomAnimal(){
+        const randomIndex = Math.floor(Math.random()*animals.length);
+        return animals[randomIndex];
+    }
+
+    const handleAnimalClick = (id) =>{
+        if(id === targetAnimal.id){
+            setResult("Win");
+        } else {
+            setResult("Lose");
+        }
+    };
+
+    const restartGame = () => {
+        setTargetAnimal(getRandomAnimal());
+        setResult(null);
+    };
+
+
+
+    return(
+      <>
+        <div className="game-page">
+            <div>
+                <hr/>
+                <h1>Animal Matching Game</h1>
+                <hr/>
+            </div>
+            <div>
+                <div>
+                    {result && (
+                        <div className="result">
+                            <h2>Result</h2>
+                            <h2>{result}</h2>
+                            <button onClick={restartGame}>Play Again</button>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            <div className="game-header">
+                <h2>Animal Name: {targetAnimal.name}</h2>
+            </div>
+
+            <div className="animal-grid">
+                {
+                    animals.map((animal) => (
+                        <div key={animal.id} className="animal-card" onClick={() => handleAnimalClick(animal.id)}>
+                          <img src={animal.img} alt={animal.name} />     
+                        </div>
+                    ))
+                }
+            </div>
+          </div>      
+      </>  
+    );
+}
+
+
